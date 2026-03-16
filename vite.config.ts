e174f89dd072aba9436db96ad44136e6e41531b2
DeadJustLive <matiasretamalbarrera.45@gmail.com>
2026-03-16 15:02:38 -0300

    Limpieza de repo y configuracion de despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/demos/saas/',
+    base: '/DemoBibliotecaERS/',
     plugins: [react(), tailwindcss()],
 })
